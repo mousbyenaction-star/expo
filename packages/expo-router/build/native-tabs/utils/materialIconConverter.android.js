dd9f94596b234866a67a441e71b9841c7590d345
Jakub Tkacz <jtkacz@expo.io>
2025-11-14 15:50:16 +0100

    [expo-router] use expo-symbols

--- packages/expo-router/build/native-tabs/utils/materialIconConverter.android.js
+++ packages/expo-router/build/native-tabs/utils/materialIconConverter.android.js
@@ -1,13 +1,24 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.convertMaterialIconNameToImageSource = convertMaterialIconNameToImageSource;
-const MaterialIcons_1 = __importDefault(require("@expo/vector-icons/MaterialIcons"));
+const expo_font_1 = require("expo-font");
+const internal_1 = require("expo-symbols/internal");
 const icon_1 = require("./icon");
 const elements_1 = require("../common/elements");
 function convertMaterialIconNameToImageSource(name) {
-    return (0, icon_1.convertComponentSrcToImageSource)(<elements_1.NativeTabsTriggerVectorIcon family={MaterialIcons_1.default} name={name}/>);
+    return (0, icon_1.convertComponentSrcToImageSource)(<elements_1.NativeTabsTriggerPromiseIcon loader={() => loadAsyncMaterialIcon(name)}/>);
+}
+async function loadAsyncMaterialIcon(name) {
+    const symbol = (0, internal_1.androidSymbolToString)(name);
+    if (!symbol) {
+        return null;
+    }
+    await (0, expo_font_1.loadAsync)({ [internal_1.materialRegularWeight.name]: internal_1.materialRegularWeight.font });
+    const renderToImageResult = await (0, expo_font_1.renderToImageAsync)(symbol, {
+        fontFamily: internal_1.materialRegularWeight.name,
+        size: 48,
+        color: 'white',
+    });
+    return renderToImageResult;
 }
 //# sourceMappingURL=materialIconConverter.android.js.map
--- packages/expo-router/src/native-tabs/utils/materialIconConverter.android.tsx
+++ packages/expo-router/src/native-tabs/utils/materialIconConverter.android.tsx
@@ -1,12 +1,30 @@
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { loadAsync, renderToImageAsync } from 'expo-font';
+import { androidSymbolToString, materialRegularWeight } from 'expo-symbols/internal';
+import { type ImageSourcePropType } from 'react-native';
 
 import { convertComponentSrcToImageSource } from './icon';
-import { NativeTabsTriggerVectorIcon, type MaterialIcon } from '../common/elements';
+import { NativeTabsTriggerPromiseIcon, type MaterialIcon } from '../common/elements';
 
 export function convertMaterialIconNameToImageSource(
-  name: MaterialIcon['material']
+  name: MaterialIcon['md']
 ): ReturnType<typeof convertComponentSrcToImageSource> {
   return convertComponentSrcToImageSource(
-    <NativeTabsTriggerVectorIcon family={MaterialIcons} name={name} />
+    <NativeTabsTriggerPromiseIcon loader={() => loadAsyncMaterialIcon(name)} />
   );
+}
+
+async function loadAsyncMaterialIcon(
+  name: MaterialIcon['md']
+): Promise<ImageSourcePropType | null> {
+  const symbol = androidSymbolToString(name);
+  if (!symbol) {
+    return null;
+  }
+  await loadAsync({ [materialRegularWeight.name]: materialRegularWeight.font });
+  const renderToImageResult = await renderToImageAsync(symbol, {
+    fontFamily: materialRegularWeight.name,
+    size: 48,
+    color: 'white',
+  });
+  return renderToImageResult;
 }
